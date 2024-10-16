9d8279d12c79fd0c49c63e3d2b8bc078a3f3db06
Jacob <calypso@jacobs-mbp.lan>
2024-10-15 23:00:39 -0400

    Added Moon & Updated Background Image

--- main.js
+++ main.js
@@ -1,71 +1,21 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import gsap from 'gsap';
-// import { viewport } from 'three/webgpu';
-
-//Loader
-// const loader = new GLTFLoader();
-
-// const dracoLoader = new DRACOLoader();
-// dracoLoader.setDecoderPath( '/examples/jsm/libs/draco/' );
-// loader.setDRACOLoader( dracoLoader );
 
 let clock = new THREE.Clock(); // # THREE SMOKE
 let delta = 0; // # THREE SMOKE
 
-
-
-
-
-
-
-//Scene 
 const scene = new THREE.Scene();
 
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+// #region Smoke Init
+
 scene.fog = new THREE.Fog(0xc0f0ff, 0.0015)// # THREE SMOKE // Color, Near
-
-
-
-
-// Load a glTF resource
-// loader.load(
-// 	// resource URL
-// 	'models/Cat/3D_Cat_Trimmed_White.gltf',
-// 	// called when the resource is loaded
-// 	function ( gltf ) {
-
-// 		scene.add( gltf.scene );
-//     gltf.scene.scale.set(40, 40, 40); 
-// 		gltf.animations; // Array<THREE.AnimationClip>
-// 		gltf.scene; // THREE.Group
-// 		gltf.scenes; // Array<THREE.Group>
-// 		gltf.cameras; // Array<THREE.Camera>
-// 		gltf.asset; // Object
-
-// 	},
-// 	// called while loading is progressing
-// 	function ( xhr ) {
-
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-// 	},
-// 	// called when loading has errors
-// 	function ( error ) {
-
-// 		console.log(error);
-
-// 	}
-// );
-
-// Create Sphere
-// // !The shape
-// const material = new THREE.TextureLoader().load( "textures/water.jpg" );
-// material.wrapS = THREE.RepeatWrapping;
-// material.wrapT = THREE.RepeatWrapping;
-// material.repeat.set( 4, 4 );
-
-const texture = new THREE.TextureLoader().load( "models/2k_earth_daymap.jpg" );
 const smokeTexture = new THREE.TextureLoader().load("models/smoke/webp/White_Cloud.webp")// #Smoke Texture
 smokeTexture.colorSpace = "srgb"; // #Smoke Texture
 const smokeGeometry = new THREE.PlaneGeometry(300,300);  // #Smoke Texture
@@ -74,7 +24,7 @@ const smokeMaterial = new THREE.MeshLambertMaterial({
     color: 0x0000ff, // vertex color = color x colormap
     map: smokeTexture,
     emissive: 0x222222, // color of emissive light
-    opacity: 0.15,
+    opacity: 0.10,
     transparent: true
 });
 
@@ -86,7 +36,7 @@ for (let i = 0; i < 90; i++) { //Can Adjust Number (90)
   let smokeElement = new THREE.Mesh(smokeGeometry, smokeMaterial);
   smokeElement.scale.set(.1, .1, .1); // Set Scale to Double Scale
   // Position smoke "Mirrors" at random location
-  smokeElement.position.set(Math.random() * 100 - 50, Math.random() * 100 - 50, Math.random() * -50 - 10)
+  smokeElement.position.set(Math.random() * 100 - 50, Math.random() * 100 - 50, Math.random() * -80)
   smokeElement.rotation.z = Math.random() * 360;
   // i % 2 === 0 ? smokeElement.rotation.y = 180 : smokeElement.rotation.y = 0
 
@@ -94,7 +44,7 @@ for (let i = 0; i < 90; i++) { //Can Adjust Number (90)
   // smokeMaterial.color = 
   smokeParticles.push(smokeElement); //add to array of smoke textures
 }
-
+// #Smoke 
 let smokeParticles2 = []  // #Smoke Texture
 // Iterates through smoke 'Mirrors'
 for (let i = 0; i < 90; i++) { //Can Adjust Number (90)
@@ -110,28 +60,45 @@ for (let i = 0; i < 90; i++) { //Can Adjust Number (90)
   smokeParticles2.push(smokeElement); //add to array of smoke textures
 }
 
+// #endregion Smoke Init
+// #region Earth Init
+const earthTexture = new THREE.TextureLoader().load( "models/2k_earth_daymap.jpg" );
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 // !The Color
 const material = new THREE.MeshStandardMaterial({
     color: '#00ffa3',
     roughness: 0.5,
-    map: texture,
+    map: earthTexture,
     emissive: 0x22222
 })
 // !The Combination of Color and Shape
-const mesh = new THREE.Mesh(geometry, material);
-// Add mesh to scene
-scene.add(mesh);
+const earth = new THREE.Mesh(geometry, material);
+// Add earth to scene
+scene.add(earth);
+// #endregion Earth Init
+// #region Moon Init
+const moonTexture = new THREE.TextureLoader().load( "models/Moon_Texture_Map_Compact.jpg" );
+const moonGeometry = new THREE.SphereGeometry(.5, 64, 64);
+// !The Color
+const moonMaterial = new THREE.MeshStandardMaterial({
+    // color: '#00ffa3',
+    roughness: 0.5,
+    map: moonTexture,
+    emissive: 0x22222
+})
+// !The Combination of Color and Shape
+const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+// Add earth to scene
+earth.add(moon);
+moon.position.x = 7
+moon.rotation.y = Math.PI
 
-//Sizes
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-}
-
-
-
-
+const moonObj = new THREE.Object3D();
+moonObj.add(moon)
+scene.add(moonObj)
+moonObj.rotation.x = Math.PI / 7
+//#endregion Moon Init
+// #region Lights, Camera, Action
 //Let there be Light
 const light = new THREE.PointLight(0xffffff, 100, 100)
 light.position.set(0, 10, 10)
@@ -170,8 +137,8 @@ controls.enableZoom = false
 controls.autoRotate = true
 controls.autoRotateSpeed = 5
 
-
-
+//#endregion Lights, Camera, Action
+// #region Resize Function
 // Resize
 window.addEventListener('resize', () => {
   //Update Sizes
@@ -183,13 +150,19 @@ window.addEventListener('resize', () => {
   camera.updateProjectionMatrix()
   renderer.setSize(sizes.width, sizes.height)
   renderer.render(scene, camera)
-  // mesh.position.x = window.innerWidth / 4 - 100
+  // earth.position.x = window.innerWidth / 4 - 100
 })
 
+//  #endregion Resize Function
+// #region Renderer Loop
 const loop = () => {
   controls.update()
   renderer.render(scene, camera);
   window.requestAnimationFrame(loop)
+  earth.rotateY(0.004)
+  moonObj.rotateY(0.005)
+  // moonObj.rotateX(0.001)
+  // moonObj.rotateZ(0.007)
   // #Smoke Texture
   delta = clock.getDelta(); //amount of time passed since last clock update  // #Smoke Texture
   for (let i=0; i < smokeParticles.length; i++) {
@@ -203,14 +176,16 @@ const loop = () => {
 
 loop()
 
+//#endregion Renderer Loop
+// #region Animation Magic
 // Timeline Magic
 const tl = gsap.timeline({defaults: { duration: 1}})
-tl.fromTo(mesh.scale, {z: 0, x: 0, y: 0}, {z: 1, x: 1, y: 1})
-tl.duration(5)
-tl.fromTo('nav', {y: '-100%'}, {y: '0%'})
+tl.duration(7)
+tl.fromTo(earth.scale, {z: 0, x: 0, y: 0}, {z: 1, x: 1, y: 1})
 tl.duration(1)
+tl.fromTo('nav', {y: '-150%'}, {y: '0%'})
 tl.fromTo('.title', {opacity: 0}, {opacity: 1})
-// Tween.to(mesh.rotation, 10, {y: Math.PI * 2, repeat: -1, ease: Linear.easeNone});
+// Tween.to(earth.rotation, 10, {y: Math.PI * 2, repeat: -1, ease: Linear.easeNone});
 
 // Mouse Animation Colorrrrr
 let mouseDown = false;
@@ -226,10 +201,13 @@ window.addEventListener('pointermove', (e) => {
       150,
     ]
 
-    // mesh.rotation.x = Math.PI / (e.pageX / sizes.width + 1);
+    // earth.rotation.x = Math.PI / (e.pageX / sizes.width + 1);
 
     //Let's Animate!
     let newColor = new THREE.Color(`rgb(${rgb.join(',')})`)
-    gsap.to(mesh.material.color, {r: newColor.r, g: newColor.g, b: newColor.b})
+    gsap.to(earth.material.color, {r: newColor.r, g: newColor.g, b: newColor.b})
   }
 })
+
+// #endregion Animation Magic
+
